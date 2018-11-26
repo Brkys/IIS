5148c9c2553643638dd213b21ea406118e402794
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-11-26 18:08:56 +0100

    Testing of bootstrap

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,12 @@
 <html>
 <head>
 	<title>IS Mafie</title>
+	<base href="{{ URL:asset('/') }}" target="_blank">
+	<link rel="stylesheet" type="text/css" href="{{ url(css/bootstrap.min.css') }}"><link rel="stylesheet" type="text/css" href="{{ url(css/styles.css') }}">
 </head>
 <body>
 	Hello world
+
+	<script src="{{ url('js/bootstrap.min.js') }}"></script>
 </body>
 </html>
