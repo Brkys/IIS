d2189f0e142f7bae301c97c26137a952a2b80525
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-11-26 21:13:51 +0100

    Test

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,50 @@
 <!DOCTYPE html>
-<html>
+<html lang="cz">
 <head>
-	<title>IS Mafie</title>
-	<base href="{{ URL:asset('/') }}" target="_blank">
-	<link rel="stylesheet" type="text/css" href="{{ url(css/bootstrap.min.css') }}"><link rel="stylesheet" type="text/css" href="{{ url(css/styles.css') }}">
-</head>
-<body>
-	Hello world
+	<title>Mafie</title>
 
-	<script src="{{ url('js/bootstrap.min.js') }}"></script>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./styles.css">
+
+	<!-- Bootstrap CSS --> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body background="black">
+	<!-- Navigator panel -->
+	<nav class="navbar navbar-expand-lg navbar-dark" id="navbar" style="background-color: #262626">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+
+	  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    	<ul class="navbar-nav mr-auto">
+	      		<li class="nav-item active">
+	    	    	<a class="nav-link" href="#">Domů <span class="sr-only">(current)</span></a>
+	   		   </li>
+	   	    	<li class="nav-item">
+	     		   <a class="nav-link" href="#">Novinky</a>
+	      		</li>
+	      		<li class="nav-item dropdown">
+	        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Přehled</a>
+	        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          			<a class="dropdown-item" href="#">Rodiny</a>
+	          			<a class="dropdown-item" href="#">Členové</a>
+	          			<div class="dropdown-divider"></div>
+	          			<a class="dropdown-item" href="#">Území</a>
+	        		</div>
+	      		</li>
+	    	</ul>
+    		<span class="navbar-text my-2 my-sm-0 pr-1">
+			    <a class="btn btn-sm my-2 my-sm-0" href="#">Registrovat se</a>
+			</span>
+      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Příhlásit se</button>
+		</div>
+	</nav>
+
+	<!-- Bootstrap javascript -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </body>
 </html>
