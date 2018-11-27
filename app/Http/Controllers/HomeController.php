3ab6ba0c8c245736968c4396f9fe4d8d4a234df6
xkaspa40 <pekaspar@gmail.com>
2018-11-27 17:50:59 +0100

    modal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,7 +13,12 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
+    }
+
+    public function home()
+    {
+        return view('home');
     }
 
     /**
