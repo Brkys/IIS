9ff631bbcefff6346371d6795aecb96b98a708a5
xkaspa40 <pekaspar@gmail.com>
2018-11-27 18:03:16 +0100

    modal

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,11 +1,24 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
+
 class HomeController extends Controller
 {
-    //Homepage
+ 
     public function home()
     {
-    	return view('home');
+        return view('home');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
     }
 }
