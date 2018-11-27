dd2a0c88f92f452d7d23b985a59a03fdf9e8f637
xkaspa40 <pekaspar@gmail.com>
2018-11-27 22:13:08 +0100

    routing

---
+++ resources/views/test.blade.php
@@ -0,0 +1,91 @@
+  
+<!DOCTYPE html>
+<html lang="cz">
+<head>
+	<title>Route Test</title>
+
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
+
+	<!-- Bootstrap CSS --> 
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body background="black">
+
+	<!-- Navigator panel -->
+	<nav class="navbar navbar-expand-lg navbar-dark" id="navbar" style="background-color: #262626">
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		</button>
+
+	  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    	<ul class="navbar-nav mr-auto">
+	      		<li class="nav-item active">
+	    	    	<a class="nav-link" href="#">Domů <span class="sr-only">(current)</span></a>
+	   		   </li>
+	   	    	<li class="nav-item">
+	     		   <a class="nav-link" href="#">Novinky</a>
+	      		</li>
+	      		<li class="nav-item dropdown">
+	        		<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Přehled</a>
+	        		<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+	          			<a class="dropdown-item" href="#">Rodiny</a>
+	          			<a class="dropdown-item" href="#">Členové</a>
+	          			<div class="dropdown-divider"></div>
+	          			<a class="dropdown-item" href="#">Území</a>
+	        		</div>
+	      		</li>
+	    	</ul>
+    		<span class="navbar-text my-2 my-sm-0 pr-1">
+			    <a class="btn btn-sm my-2 my-sm-0" href="#">Registrovat se</a>
+			</span>
+      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit" data-toggle="modal" data-target="#loginModal">Příhlásit se</button>
+		</div>
+	</nav>
+
+
+	<!-- Modal window --> 
+	<	<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labeledby="modal_login">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modal_login">Login</h5>
+
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+			</div>
+			<div class="modal-body" id="modal_body">
+				<label>Uživatelské jméno</label>
+				<input type="text" name="username" id="username" class="form-control"/>
+				<br/>
+				<label>Heslo</label>
+				<input type="password" name="password" id="password" class="form-control" />
+				<br/>
+				<button type="button" name="login_btn" id="login_btn" class="btn btn-outline-success">Login</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<script>
+$(document).ready(function(){
+	$('#login_btn').click(funtion(){
+		var username = $('#username').val();
+		var password = $('#password').val();
+		if(username != '' && password != ''){
+			
+		}
+		else
+		{
+			alert("Chybne jmeno nebo heslo!");
+		}
+	});
+});
+</script>
+
+	<!-- Bootstrap javascript -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+</body>
+</html>
