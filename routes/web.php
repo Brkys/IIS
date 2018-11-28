d79063c200a076c478739925d74d3c9ae4baffc9
xkaspa40 <pekaspar@gmail.com>
2018-11-28 22:47:59 +0100

    modal

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Auth::routes();
 
 Route::get('/test', 'TestController@test');
 
-Route::post('/loginU', 'LoginController@index')->name('login');
+Route::post('/loginU', 'LoginController@login');
 
 Route::get('/login', function(){
     return redirect('home')->with('openLogin', true);
