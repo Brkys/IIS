ed5ace17e3b2cf8c7f096e0a8c0cef62bd0ff654
xkaspa40 <pekaspar@gmail.com>
2018-11-28 23:39:32 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,8 +54,8 @@
 				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 			</div>
 			<div class="modal-body" id="modal_body">
-  				<form action="form" method="post">
-				  
+  				<form action="login" method="post">
+
                     <label>Uživatelské jméno</label>
                     <input type="text" name="username" id="username" class="form-control"/>
                     <br/>
