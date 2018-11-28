8a764aad4820aa6c721ce2a2c1cd8487175b5744
xkaspa40 <pekaspar@gmail.com>
2018-11-28 18:32:26 +0100

    modal

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -71,8 +71,8 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">×</span></button>
-                <h4 class="modal-title" id="registerModalLabel">Register</h4>
+                <h5 class="modal-title" id="registerModalLabel">Register</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
  
