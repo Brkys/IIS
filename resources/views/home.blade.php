24eb4ee134d886e6f0a1c0e21cdcb39dad388e31
xkaspa40 <pekaspar@gmail.com>
2018-11-29 00:17:08 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,7 +28,7 @@
 	      		</li>
 				  @if(isset($data))
 				  <li class="nav-item">
-  					<a class="nav-link" href="">{{ $data }}</a>
+  					<a class="nav-link" href="">Neco</a>
 				  </li>
 				  @endif
 	      		<li class="nav-item dropdown">
