9f59c3070342d98f7253a2aa275878c06375e11b
xkaspa40 <pekaspar@gmail.com>
2018-11-29 15:06:44 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,13 +51,14 @@
 		</div>
 	</nav>
 
-	<?php 
-	try {
-		DB::connection()->getPdo();
-	} catch (Exception $e) {
-		die("Could not connect to the database.  Please check your configuration. error:" . $e );
-	}
-	php>
+	
+	<?php
+            try {
+                DB::connection()->getPdo();
+            } catch (\Exception $e) {
+                die("Could not connect to the database.  Please check your configuration. error:" . $e );
+            }
+    ?>
 
 
 	<!-- Modal login --> 
