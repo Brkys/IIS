60ea5ff38374371cab3640fe7e8405449b8745dd
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-11-29 18:17:57 +0100

    Adding home text

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -137,18 +137,20 @@
 </div>
 
 <!-- Content of page -->
-<div style="height: 100%">
-	<img src="../images/header.jpg" width="50%" height="100%" style="margin-top: 50px;">
-	<div class="fade-in h1" >
-		Per me si va ne la città dolente, <br>
-		per me si va ne l'eterno dolore, <br>
-		per me si va tra la perduta gente. <br>
-		Giustizia mosse il mio alto fattore; <br>
-		fecemi la divina podestate, <br>
-		la somma sapïenza e 'l primo amore. <br>
-		Dinanzi a me non fuor cose create <br>
-		se non etterne, e io etterno duro. <br>
-		Lasciate ogne speranza, voi ch'intrate
+<div class="container">
+	<div class="row justify-content-md-center">
+		<div class="col col-lg-2"><img src="../images/header.jpg" width="50%" height="100%" style="margin-top: 50px;"></div>
+		<div class="fade-in h1 col-md-auto" >
+			Per me si va ne la città dolente, <br>
+			per me si va ne l'eterno dolore, <br>
+			per me si va tra la perduta gente. <br>
+			Giustizia mosse il mio alto fattore; <br>
+			fecemi la divina podestate, <br>
+			la somma sapïenza e 'l primo amore. <br>
+			Dinanzi a me non fuor cose create <br>
+			se non etterne, e io etterno duro. <br>
+			Lasciate ogne speranza, voi ch'intrate
+		</div>
 	</div>
 </div>
 
