3241f4b61ade231cdc89cd597651a388c5139b87
xkaspa40 <pekaspar@gmail.com>
2018-11-29 16:33:01 +0100

    modal

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -11,16 +11,7 @@ class loginController extends Controller
     public function login(Request $req)
     {
         $model = new userModel();
-        /*try {
-            DB::connection()->getPdo();
-            if(DB::connection()->getDatabaseName()){
-                echo "Yes! Successfully connected to the DB: " . DB::connection()->getDatabaseName();
-            }else{
-                die("Could not find the database. Please check your configuration.");
-            }
-        } catch (\Exception $e) {
-            die("Could not open connection to database server.  Please check your configuration.");
-        }*/
+        DB::connection()->getPdo();
         $data = array("name" => $req->input('username'), "pwd" => $req->input('password'));
         return view('home')->with('data', $data);
     }
