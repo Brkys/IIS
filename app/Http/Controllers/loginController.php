52fc6a73d0187ded8a3b7c37aa1640f1dcd18c86
xkaspa40 <pekaspar@gmail.com>
2018-11-29 18:24:51 +0100

    modal

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -17,7 +17,7 @@ class loginController extends Controller
         $data = -1;
         $model = $model::all();
         forech $model as $mod{
-            echo($mod['name']);
+            echo($mod);
         }
         $data = array("name" => $req->input('username'), "pwd" => $req->input('password'));
         return view('home')->with('data', $data);
