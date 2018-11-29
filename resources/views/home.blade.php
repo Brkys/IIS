fbc16589bdf72def69e9981f857d21210c8ed0ac
xkaspa40 <pekaspar@gmail.com>
2018-11-29 14:56:11 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,7 +54,7 @@
 	<?php 
 	try {
 		DB::connection()->getPdo();
-	} catch (\Exception $e) {
+	} catch (Exception $e) {
 		die("Could not connect to the database.  Please check your configuration. error:" . $e );
 	}
 	php>
