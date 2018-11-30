c79a7052406fe472b63c43366ed9136733e5f82d
xkaspa40 <pekaspar@gmail.com>
2018-11-30 20:05:27 +0100

    modal

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -18,11 +18,11 @@ class loginController extends Controller
         $pwd = $req->input('password');
         $found = userModel::FindUser($name, $pwd);
         if($found === true){
-            session_start();
+            /*session_start();
             $_SESSION['loggedIn'] = true;
             $_SESSION['id'] = $value->id;
             $_SESSION['username'] = $value->name;
-            $_SESSION['permission'] = $value->permission;
+            $_SESSION['permission'] = $value->permission;*/
             return redirect('home');
         }
         //login se nepodaril
