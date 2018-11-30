066bb0793ff32d2663caf6193adf825c6b056b7f
xkaspa40 <pekaspar@gmail.com>
2018-11-30 17:17:21 +0100

    modal

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -10,20 +10,20 @@ class loginController extends Controller
  
     public function login(Request $req)
     {
-        $model = new userModel();           //vytvoreni nove instance modelu 
+        $model = new userModel();
         $name = $req->input('username');
         $pwd = $req->input('password');
         $loginCorrect = false;
         $model = $model::all();
         foreach ($model as $key => $value) {
             if($value->name == $name && $value->password == $pwd){
-                /*session_start();
+               /* session_start();
                 $_SESSION['logedIn'] = true;
                 $_SESSION['id'] = $value->id;
                 $_SESSION['username'] = $value->name;
                 return redirect('home');*/
             }
         }
-        return redirect()->route('home')->with('notLogged', true);
+        return view('home')->with('notLogged', true);
     }
 }
