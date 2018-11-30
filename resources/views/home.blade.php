702feac3aa838c3649fd077100e0a72a87eda01f
xkaspa40 <pekaspar@gmail.com>
2018-11-30 20:29:42 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -47,15 +47,13 @@
     		<span class="navbar-text my-2 my-sm-0 pr-1">
 			    <a class="btn btn-sm my-2 my-sm-0" href="#" data-toggle="modal" data-target="#registerModal">Registrovat se</a>
 			</span>
-			<!--@if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true)-->
+			@if(!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true)
       		<button class="btn btn-outline-success my-2 my-sm-0" type="button" data-toggle="modal" data-target="#loginModal">Příhlásit se</button>
-			<!--@else
+			@else
 			<button class="btn btn-outline-success my-2 my-sm-0" type="button">Odhlásit se</button>
-			@endif-->
+			@endif
 		</div>
 	</nav>
-
-
 
 	<!-- Modal login --> 
 	<div class="modal fade" id="loginModal" tabindex="-1" role="dialog" aria-labeledby="modal_login">
