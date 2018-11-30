4c01452e2c807a631c40dc4705df802b6833506d
xkaspa40 <pekaspar@gmail.com>
2018-11-30 20:47:21 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,7 +148,7 @@
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
 
-@if(!empty(Session::get('openLogin')) && Session::get('openLogin') == true) 
+@if(!empty(Session::get('openLogin')) && Session::get('openLogin') == true && empty(Session::get('loggedIn')))
 <script>
 $(document).ready(function() {
 $("#loginModal").modal('show');
