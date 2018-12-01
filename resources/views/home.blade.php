da5ad672cd0c26970ed378752c1d2eb68c9f6961
xkaspa40 <pekaspar@gmail.com>
2018-12-01 19:27:48 +0100

    modal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -163,13 +163,6 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
-
-<script>
-$(document).ready(function () {
-	$("#registerModal").modal('show');
-});
-</script>
-
 @if((!empty(Session::get('openLogin')) && Session::get('openLogin') == true && (!isset($_SESSION["loggedIn"]) || $_SESSION["loggedIn"] !== true)) ||!empty(Session::get('validationFail')) && Session::get('validationFail') == true)
 <script>
 $(document).ready(function() {
