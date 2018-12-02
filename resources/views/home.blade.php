2120fcd4d6b4a72b9bca4ad03402b1fbaffc038b
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-02 01:53:09 +0100

    Added table for news

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -136,35 +136,37 @@
 </div>
 
 <!-- Content of page -->
-<table class="table table-hover table-dark">
-  	<thead>
-    	<tr>
-      		<th scope="col">#</th>
-		    <th scope="col">First</th>
-		    <th scope="col">Last</th>
-		    <th scope="col">Handle</th>
-    	</tr>
-	</thead>
-	<tbody>
-	    <tr>
-	        <th scope="row">1</th>
-	        <td>Mark</td>
-	        <td>Otto</td>
-	        <td>@mdo</td>
-	    </tr>
-	    <tr>
-	        <th scope="row">2</th>
-	        <td>Jacob</td>
-  	        <td>Thornton</td>
-	        <td>@fat</td>
-	    </tr>
-	    <tr>
-	        <th scope="row">3</th>
-	        <td colspan="2">Larry the Bird</td>
-	        <td>@twitter</td>
-	    </tr>
-	</tbody>
-</table>
+<div style="margin-top: 50px; width: 80%;">
+	<table class="table table-hover table-dark">
+	  	<thead>
+	    	<tr>
+	      		<th scope="col">#</th>
+			    <th scope="col">First</th>
+			    <th scope="col">Last</th>
+			    <th scope="col">Handle</th>
+	    	</tr>
+		</thead>
+		<tbody>
+		    <tr>
+		        <th scope="row">1</th>
+		        <td>Mark</td>
+		        <td>Otto</td>
+		        <td>@mdo</td>
+		    </tr>
+		    <tr>
+		        <th scope="row">2</th>
+		        <td>Jacob</td>
+	  	        <td>Thornton</td>
+		        <td>@fat</td>
+		    </tr>
+		    <tr>
+		        <th scope="row">3</th>
+		        <td colspan="2">Larry the Bird</td>
+		        <td>@twitter</td>
+		    </tr>
+		</tbody>
+	</table>
+</div>
 
 	<!-- Bootstrap javascript -->
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
