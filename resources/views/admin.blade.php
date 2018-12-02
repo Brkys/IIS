9822eebbcf42520b18b087bd120d7fd42086c318
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-03 00:30:47 +0100

    Added more items to admin page

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -2,14 +2,6 @@
 
 @section('title', 'Hlavní stránka')
 
-@section('head')
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/css/bootstrap-select.css" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.12.1/js/bootstrap-select.js"></script>
-@endsection
-
 @section('navpanel')
 	@parent
 @endsection
@@ -19,14 +11,14 @@
 <div class="container" style="width: 80% !important; background-color: white !important; border-radius: 4 !important;"> 
 	<h4 style="padding-top: 20px;">Přidat familii</h4>
 	<input class="form-control" id="myInput" type="text" placeholder="Jméno familie">
-	<h6>Don</h6>
+	<h6 style="padding-top: 20px">Don</h6>
 	<select class="form-control" id="sel1">
 	    <option>User1</option>
 	    <option>User2</option>
 	    <option>User3</option>
 	    <option>User4</option>
 	</select>
-	<h6>Consigliare</h6>
+	<h6 style="padding-top: 20px">Consigliare</h6>
 	<select class="form-control" id="sel1">
 	  	<option>User1</option>
 	    <option>User2</option>
@@ -35,23 +27,20 @@
 	</select>
 	<button type="button" class="btn btn-primary btn-block">Potvrdit</button>
 	<hr>
-	<h4 style="padding-top: 20px;">Přidat území</h4>
+	<h4 style="padding-top: 20px;">Jméno území</h4>
 	<input class="form-control" id="myInput" type="text" placeholder="Jméno familie">
-	<h6>Don</h6>
+	<h6>Majitel (volitelné)</h6>
 	<select class="form-control" id="sel1">
 	    <option>User1</option>
 	    <option>User2</option>
 	    <option>User3</option>
 	    <option>User4</option>
 	</select>
-	<h6>Consigliare</h6>
-	<select class="form-control" id="sel1">
-	  	<option>User1</option>
-	    <option>User2</option>
-	    <option>User3</option>
-	    <option>User4</option>
-	</select>
-	<button type="button" class="btn btn-primary btn-block">Potvrdit</button>
+	<h6 style="padding-top: 20px">Adresa</h6>
+	<input class="form-control" id="myInput" type="text" placeholder="Jméno familie">
+	<h6 style="padding-top: 20px">Rozloha v m^2</h6>
+	<input class="form-control" id="myInput" type="text" placeholder="Jméno familie"> <!-- check čisla -->
+	<button style="padding-top: 20px" type="button" class="btn btn-primary btn-block">Potvrdit</button>
 	<!-- CONSIGLIARE != DON -->
 
 </div>
