dfb69a1c858153786f67809fc4a79362fb4fe7a0
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-02 19:54:38 +0100

    Small fixes

--- resources/views/free-users.blade.php
+++ resources/views/free-users.blade.php
@@ -29,15 +29,13 @@
 
 <script>
 function filter() {
-  // Declare variables 
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById("myInput");
   filter = input.value.toUpperCase();
   table = document.getElementById("filterTable");
   tr = table.getElementsByTagName("tr");
-  // Loop through all table rows, and hide those who don't match the search query
   for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[1];
+    td = tr[i].getElementsByTagName("td")[0];
     if (td) {
       txtValue = td.textContent || td.innerText;
       if (txtValue.toUpperCase().indexOf(filter) > -1) {
