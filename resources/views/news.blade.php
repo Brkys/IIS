1dfacce93867b6c3f9f6972686e655e7f7d37683
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-02 14:11:51 +0100

    Testing

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -27,12 +27,12 @@
 
 <script>
 $(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    $("#myInput").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $("#myTable tr").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
     });
-  });
 });
 </script>
 @endsection
