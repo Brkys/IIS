036af6bf1bbb42fd0fa77a93cc0334b8b1a901d5
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-02 18:53:02 +0100

    ADding filter

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="container" style="padding-top: 100px; width: 80% !important;"> 
     <input class="form-control" id="myInput" type="text" placeholder="Vyhledat..">
-    <table class="table table-hover table-dark text-centered" id="custom-table">
+    <table class="table table-hover table-dark text-centered" id="custom-table filterTable">
         <thead>
             <tr>
                 <th scope="col" style="width: 20%;">Datum</th>
@@ -26,13 +26,26 @@
 </div>
 
 <script>
-$(document).ready(function(){
-    $("#myInput").on("keyup", function() {
-        var value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
+function find() {
+  // Declare variables 
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("filterTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    } 
+  }
+}
 </script>
 @endsection
