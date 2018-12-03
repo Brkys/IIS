ac7a51c04e9be9522a77b90495e89d918d7efde2
xkaspa40 <pekaspar@gmail.com>
2018-12-04 00:52:52 +0100

    autorizace - prihlaseni + permission

--- app/Http/Controllers/ShowCriminalController.php
+++ app/Http/Controllers/ShowCriminalController.php
@@ -3,11 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\userModel;
-use App\landModel;
-use App\Familie;
-use App\CProvadiK;
-use App\criminal;
 
 class ShowCriminalController extends Controller
 {
@@ -19,27 +14,6 @@ class ShowCriminalController extends Controller
         else if($_SESSION['permission'] < 0 || $_SESSION['permission'] > 4){
             return redirect('no-permission');
         }
-        else if($_SESSION['permission'] == 0){
-            return $this->showMine();
-        }
-        else {
-            return $this->showAll();
-        }
-    }
-
-    public function showMine() {
-        $CProvadi = CProvadiK::where('ID_Clena', $_SESSION['id']);
-        $cinnostiId = array();
-        foreach($CProvadi as $key => $value){
-            array_push($cinnostiId, $value->ID_Cinnosti);
-        }
-
-        $kriminal = array();
-        foreach($cinnostiId as $cinnost){
-            $kriminalni = criminal::where('ID_Cinnosti', $cinnost);
-            array_push($kriminal, $kriminalni);
-        }
-
-        
+        else return view('show-criminal');
     }
 }
