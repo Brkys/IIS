ca6c1e1b02675da8d666d0a7cf1395381a5d9ed6
xkaspa40 <pekaspar@gmail.com>
2018-12-03 17:11:09 +0100

    autorizace - prihlaseni + permission

--- app/Http/Controllers/FreeUsersController.php
+++ app/Http/Controllers/FreeUsersController.php
@@ -16,7 +16,16 @@ class FreeUsersController extends Controller
             return redirect('home');
         }
         else {
+            $invitations = Invitation::all();
             $freeUsers = userModel::FreeUser();
+            foreach($freeUsers as &$users){
+                foreach($invitations as $invitation){
+                    if($users['id'] == $invitation->ID_Uzivatele && $invitation->ID_Familie == $_SESSION['familia']){
+                        $users['invited'] = true;
+                    }
+                }
+            }
+            unset($users);
             return view('free-users')->with('freeUsers', $freeUsers);
         }
     }
