919b6adeb72bd87ed2ec7639b02be9cd83e15548
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-03 10:47:22 +0100

    Added create crminial interface

--- resources/views/create-criminal.blade.php
+++ resources/views/create-criminal.blade.php
@@ -11,21 +11,23 @@ ID_Cinnosti	ID_Uzemi	TypCinnosti	DatumZacatku	DatumUkonceni
 
 <div style="padding-top: 100px"></div>
 <div class="container" style="width: 80% !important; background-color: white !important; border-radius: 4 !important;"> 
-	<h4 style="padding-top: 20px;">Jméno</h4>
-	<input class="form-control" id="myInput" type="text" placeholder="Jméno"> <!-- GET Jméno/úprava? -->
-	<h6 style="padding-top: 20px">Přezdívka</h6>
-	<input class="form-control" id="myInput" type="text" placeholder="Přezdívka"> <!-- GET Přezdívka/úprava? -->
-	<h6 style="padding-top: 20px">Úroveň účtu</h6>
-
-	<button type="button" class="btn btn-primary btn-block">Potvrdit</button>
-	<div style="padding-top: 20px"></div>
-	<script type="text/javascript">	$('select').selectpicker();</script>
+	<h4 style="padding-top: 20px;">Území</h4>
+	<h6>
+		<select style="width: 100%;">
+		    <option>Území 1</option>
+		    <option>Území 2</option>
+	    	<option>Území 3</option>
+	  	</select>
+	</h6>
+	<h4 style="padding-top: 20px;">Popis činnosti</h4>
+	<input class="form-control" id="myInput" type="text" placeholder="Typ činnosti"> 
+	<h6 style="padding-top: 20px">Datum začátku</h6>
+    <div class="input-group mb-3">
+        <div class="input-group-prepend">
+            <span class="input-group-text">YYYY-MM-DD</span>
+        </div>
+		<input class="form-control" id="myInput" type="text" placeholder="Datum"> 
+    </div>
 
 	<!-- do přidávání do tabulky zakomponovat i datum začátku, což by mělo být now() -->
-
-  <select style="width: 100%">
-    <option>Here is the first option</option>
-    <option>The second option</option>
-    <option>The third option</option>
-  </select>
 @endsection
