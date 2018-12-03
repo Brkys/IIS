980048e0dc1ac2d78d3547ed4ecf964f2be374bd
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-03 17:25:56 +0100

    Adding show criminal page

--- resources/views/show-criminal.blade.php
+++ resources/views/show-criminal.blade.php
@@ -7,5 +7,30 @@
 @endsection
 
 @section('content')
-<img src="../images/header.jpg" width="50%" height="100%" style="margin-top: 50px;">
+<div class="container" style="padding-top: 100px; width: 80% !important;"> 
+    <table class="table table-hover table-dark text-centered" id="filterTable">
+        <thead>
+            <tr>
+                <th scope="col" style="width: 15%;">Území</th>
+                <th scope="col" style="width: 15%;">Familie</th>
+                <th scope="col" style="width: 40%;">Popis činnosti</th>
+                <th scope="col" style="width: 10%;">Datum začátku</th>
+                <th scope="col" style="width: 10%;">Datum ukončení</th>
+                <th scope="col" style="width: 5%;">Počet podílejících se členů</th>
+                <th scope="col" style="width: 5%;">Podílím se?</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Brno</td>
+                <td>Quatro formagi</td>
+                <td>Obsazení území</td>
+                <td>23.2.1997</td>
+                <td>23.2.1997</td>
+                <td>264</td>
+                <td>Ano</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
 @endsection
