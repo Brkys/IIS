a25f151740ce4bceacd8fe4a7e7896d658a0481c
xkaspa40 <pekaspar@gmail.com>
2018-12-03 23:20:47 +0100

    autorizace - prihlaseni + permission

---
+++ app/CProvadiK.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CProvadiK extends Model
+{
+    /**
+     * tabulka spojena s modelem
+     */
+    public $timestamps = false;
+}
