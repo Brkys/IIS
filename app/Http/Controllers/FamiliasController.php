6a8a2ff00bf2c1031debd3a2c7cf19c3c76cb2a6
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-03 22:28:11 +0100

    a

--- app/Http/Controllers/FamiliasController.php
+++ app/Http/Controllers/FamiliasController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Familia;
+use App\userModel;
 
 class FamiliasController extends Controller
 {
@@ -12,5 +14,27 @@ class FamiliasController extends Controller
                 return redirect('home')->with('openLogin', true);
             }
             return view('familias');
+    }
+
+    public function viewFamilias() {
+    	$familias = Familia::all();
+    	$dons = array();
+    	$counts = array();
+    	$iterator = 0;
+    	$familiaNames = array();
+    	foreach ($familia as $familias) {
+    		array_push($familiaNames, $familias['JmenoFamilie'])
+    		array_push($dons, $familias['ID_Dona']);
+    		$counts[$iterator] = userModel::where('familia_id', '=', $familia['ID_Familie'])->count();
+    	}
+    	$users = Familia::all();
+    	foreach ($id as $dons) {
+    		$donNames = userModel::find($id)['name']->get();
+    	}
+    	$result = array();
+    	array_push($result, $familiaNames)
+    	array_push($result, $donNames);
+    	array_push($result, $counts);
+    	return view('familias')->with('data', $result);
     }
 }
