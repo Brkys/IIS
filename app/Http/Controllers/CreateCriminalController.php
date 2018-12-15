834c0272bc9e76e650346674848d65c33b62d496
xkaspa40 <pekaspar@gmail.com>
2018-12-15 16:55:28 +0100

    includnut validator

--- app/Http/Controllers/CreateCriminalController.php
+++ app/Http/Controllers/CreateCriminalController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\criminal;
 use App\familiaActionModel;
+use Validator;
 
 class CreateCriminalController extends Controller
 {
