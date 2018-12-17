0fdbfd92762bc76b9c740d5f763ebea1cb172b6d
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-17 23:43:34 +0100

    Added aliance page content

--- resources/views/show-alliance.blade.php
+++ resources/views/show-alliance.blade.php
@@ -7,5 +7,52 @@
 @endsection
 
 @section('content')
+<div style="padding-top: 100px"></div>
+<div class="container" style="; width: 80% !important; background-color: white !important;">
+  <form action="aliance-request" method="post">
+    @csrf
+  	<h4 style="padding-top: 20px;">Aliance</h4>
+  	<div style="padding-top: 20px"></div>
+    <input class="form-control" id="myInput" type="text" placeholder="Vyhledat podle jména.." onkeyup="filter()">
+    <table class="table table-hover table-dark text-centered" id="filterTable">
+        <thead>
+            <tr>
+                <th scope="col" style="width: 70%;">Familie</th>
+                <th scope="col" style="width: 30%;"></th>
+            </tr>
+        </thead>
+        <tbody>
+              <tr>
+                  <td></td>
+                  <td>
+                    <input type="hidden" value="TODO" name="user_id">
+                    <button type="submit" class="btn btn-primary" name="aliance-request">Nabídnout alianci</button>
+                  </td>
+              </tr>
+        </tbody>
+    </table>
+  </form> 
+  <div style="padding-top: 20px"></div>
+</div>
 
+<script>
+function filter() {
+   var input, filter, table, tr, td, i, txtValue;
+   input = document.getElementById("myInput");
+   filter = input.value.toUpperCase();
+   table = document.getElementById("filterTable");
+   tr = table.getElementsByTagName("tr");
+   for (i = 0; i < tr.length; i++) {
+   		td = tr[i].getElementsByTagName("td")[0];
+	    if (td) {
+		    txtValue = td.textContent || td.innerText;
+		    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+		        tr[i].style.display = "";
+		    } else {
+		        tr[i].style.display = "none";
+		    }
+	    } 
+    }
+}
+</script>
 @endsection
