b4a8f5afebb41311622b3e176cb56d5a794e89db
Zdeněk Brhel <brhelzdenek@seznam.cz>
2018-12-17 23:35:21 +0100

    Fixed aliance controller

--- app/Http/Controllers/Auth/AlianceController.php
+++ app/Http/Controllers/Auth/AlianceController.php
@@ -4,19 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class AllianceController extends Controller
+class AlianceController extends Controller
 {
  
     public function showAliances()
     {
         session_start();
         if(!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true){
-            return redirect('home')->with('openLogin', true);
+            return redirect('home')->with('openLogin', true);   
         }
-
-		if($_SESSION['permission'] != 4) {
-			return view('no-permission');
-		}
+        else if($_SESSION['permission'] !== 4){
+            return redirect('no-permission');
+        }
 
         return view('show-alliance');
     }
